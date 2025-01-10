25bff6e1fd26af9af9ff92ceacee1e9312d42ed5
giyushino <allanzhang440@gmail.com>
2025-01-09 18:49:52 -0800

    updated resume and some other shite

---
+++ CS31/proj1/main.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <iostream>
+
+// Main function, this is a comment!
+int main() {
+    int first, second, sum;
+    std::cout << "Enter two integers\n";
+    std::cin >> first >> second;
+    sum = first + second;
+    std::cout << "Sum:" << sum;
+
+    return sum;
+}
+
+
+
+
+
